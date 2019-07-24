6a2345e7ff1efe3532b575185f2a2f16929c7b9f
adir <adir@nuweba.com>
2019-07-24 15:40:37 +0300

    refactor sleep function

--- arsenal/sleep/google/index.js
+++ arsenal/sleep/google/index.js
@@ -1,22 +1,53 @@
-var wait = ms => new Promise((r, j)=>setTimeout(r, ms));
+var wait = ms => new Promise((r, j) => setTimeout(r, ms));
 
-async function testfunc(req, res) {
-	let startTime = process.hrtime();
-	const sleep_time = req.query.sleep ? parseInt(req.query.sleep) : null;
-	if (!sleep_time && sleep_time !== 0) {
-		res.status(500).send({error: "invalid sleep parameter"});
-	}
-	await wait(sleep_time);
-	let is_warm = process.env.warm ? true : false;
-  	process.env.warm = true
-	let end = process.hrtime(startTime);
-	
-	res.status(200).send({reused : is_warm, duration : end[1] + (end[0] * 1e9)});
-	
+function getDuration(startTime) {
+    var end = process.hrtime(startTime);
+    return end[1] + (end[0] * 1e9);
 }
 
-exports.test128 = testfunc;
-exports.test256 = testfunc;
-exports.test512 = testfunc;
-exports.test1024 = testfunc;
-exports.test2048 = testfunc;
+function getSleep(event) {
+    let sleep_time = event.query.sleep ? parseInt(event.query.sleep) : null;
+    if (!sleep_time && sleep_time !== 0) {
+        return {"error": "invalid sleep parameter"};
+    }
+    return sleep_time;
+}
+
+function getParameters(event) {
+    return getSleep(event);
+}
+
+async function runTest(sleep_time) {
+    await wait(sleep_time);
+}
+
+function isWarm() {
+    var is_warm = process.env.warm ? true : false;
+    process.env.warm = true;
+    return is_warm;
+}
+
+async function main(req, res) {
+    let startTime = process.hrtime();
+    let params = getParameters(req);
+    if (params.error) {
+        return {"error": params.error}
+    }
+
+    await runTest(params);
+
+    var reused = isWarm();
+    var duration = getDuration(startTime);
+
+    res.status(200).send({
+        reused: reused,
+        duration: duration
+    });
+
+}
+
+exports.test128 = main;
+exports.test256 = main;
+exports.test512 = main;
+exports.test1024 = main;
+exports.test2048 = main;
