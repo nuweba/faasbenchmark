0596d8c859399147b729b070e876334de0e8825b
adir <adir@nuweba.com>
2019-09-11 17:22:04 +0300

    add google go sleep test

---
+++ arsenal/sleep/google/fn.go
@@ -0,0 +1,46 @@
+package google
+
+import (
+	"errors"
+	"strconv"
+	"time"
+	"os"
+	"fmt"
+	"net/http"
+)
+
+func isWarm() bool{
+	warm := os.Getenv("warm") == "true"
+	os.Setenv("warm", "true")
+	return warm
+}
+
+func runTest(sleepTime time.Duration) {
+	time.Sleep(sleepTime)
+}
+
+func getSleepParameter(r *http.Request) (time.Duration, error) {
+	userInput := r.URL.Query().Get("sleep")
+	sleepTime, err := strconv.Atoi(userInput)
+	if err != nil || sleepTime < 0 {
+		return time.Nanosecond, errors.New("invalid sleep parameter")
+	}
+	return time.Duration(sleepTime) * time.Millisecond, nil
+}
+
+func getParameters(r *http.Request) (time.Duration, error) {
+	return getSleepParameter(r)
+}
+
+func Hello(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+	reused := isWarm()
+	sleepTime, err := getParameters(r)
+	if err != nil {
+		fmt.Fprintf(w, `{"error": "%s"}`, err.Error())
+		return
+	}
+	runTest(sleepTime)
+	duration := time.Since(start).Nanoseconds()
+	fmt.Fprintf(w, `{"reused": %t, "duration": %d}`, reused, duration)
+}
