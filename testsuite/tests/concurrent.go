3241e38f7a524a0e32d2a621192df11141a3f94e
adir <adir@nuweba.com>
2019-10-03 16:44:15 +0300

    add levels to concurrent test

--- testsuite/tests/concurrent.go
+++ testsuite/tests/concurrent.go
@@ -10,27 +10,76 @@ import (
 	"sync"
 )
 
+/*
+  We use less than the default maxConcurrent to ensure the total number of requests running concurrently is not more
+  than ~800 - the number of requests sent in a regular increasingLoad test
+*/
+const (
+	Lvl2ConcurrencyLimit = 34
+	Lvl3ConcurrencyLimit = 18
+)
+
 func init() {
-	Tests.Register(Test{Id: "ConcurrentIncreasingLoad", Fn: C10FunctionsConcurrently1Each, RequiredStack: "identicalfunctions", Description: "Test concurrent load"})
-	Tests.Register(Test{Id: "ConcurrentIncreasingLoadWindows", Fn: C10FunctionsConcurrently1Each, RequiredStack: "identicalfunctionswindows", Description: "Test concurrent load - azure functions on windows"})
+	Tests.Register(Test{Id: "ConcurrentIncreasingLoadLvl1", Fn: ConcurrentIncreasingLoadLvl1, RequiredStack: "identicalfunctions", Description: "Test concurrent load"})
+	Tests.Register(Test{Id: "ConcurrentIncreasingLoadWindowsLvl1", Fn: ConcurrentIncreasingLoadLvl1, RequiredStack: "identicalfunctionswindows", Description: "Test concurrent load - azure functions on windows"})
+	Tests.Register(Test{Id: "ConcurrentIncreasingLoadLvl2", Fn: ConcurrentIncreasingLoadLvl2, RequiredStack: "identicalfunctions", Description: "Test concurrent load"})
+	Tests.Register(Test{Id: "ConcurrentIncreasingLoadWindowsLvl2", Fn: ConcurrentIncreasingLoadLvl2, RequiredStack: "identicalfunctionswindows", Description: "Test concurrent load - azure functions on windows"})
+	Tests.Register(Test{Id: "ConcurrentIncreasingLoadLvl3", Fn: ConcurrentIncreasingLoadLvl3, RequiredStack: "identicalfunctions", Description: "Test concurrent load"})
+	Tests.Register(Test{Id: "ConcurrentIncreasingLoadWindowsLvl3", Fn: ConcurrentIncreasingLoadLvl3, RequiredStack: "identicalfunctionswindows", Description: "Test concurrent load - azure functions on windows"})
 }
 
-func C10FunctionsConcurrently1Each(test *config.Test) {
-	graphConcurrencyLimit := 34
+func ConcurrentIncreasingLoadLvl1(test *config.Test){
+	runtime := shortRuntime
 	headers := http.Header{}
 	body := []byte("")
-	params := sleepQueryParam(mediumRuntime)
-
+	params := sleepQueryParam(runtime)
 	httpConfig := &config.Http{
-		SleepTime:   mediumRuntime,
+		SleepTime:   runtime,
 		QueryParams: params,
 		TestType:    httpbench.RequestsForTimeGraph.String(),
-		HitsGraph:   gradualHitGraph(graphConcurrencyLimit, Lvl2),
+		HitsGraph:   gradualHitGraph(maxConcurrent, Lvl1),
 		Hook:        test.Config.Provider.HttpInvocationTriggerStage(),
 		Body:        &body,
 		Headers:     &headers,
 	}
+	testConcurrently(test, httpConfig)
+}
 
+func ConcurrentIncreasingLoadLvl2(test *config.Test){
+	runtime := mediumRuntime
+	headers := http.Header{}
+	body := []byte("")
+	params := sleepQueryParam(runtime)
+	httpConfig := &config.Http{
+		SleepTime:   runtime,
+		QueryParams: params,
+		TestType:    httpbench.RequestsForTimeGraph.String(),
+		HitsGraph:   gradualHitGraph(Lvl2ConcurrencyLimit, Lvl2),
+		Hook:        test.Config.Provider.HttpInvocationTriggerStage(),
+		Body:        &body,
+		Headers:     &headers,
+	}
+	testConcurrently(test, httpConfig)
+}
+
+func ConcurrentIncreasingLoadLvl3(test *config.Test){
+	runtime := longRuntime
+	headers := http.Header{}
+	body := []byte("")
+	params := sleepQueryParam(runtime)
+	httpConfig := &config.Http{
+		SleepTime:   runtime,
+		QueryParams: params,
+		TestType:    httpbench.RequestsForTimeGraph.String(),
+		HitsGraph:   gradualHitGraph(Lvl3ConcurrencyLimit, Lvl3),
+		Hook:        test.Config.Provider.HttpInvocationTriggerStage(),
+		Body:        &body,
+		Headers:     &headers,
+	}
+	testConcurrently(test, httpConfig)
+}
+
+func testConcurrently(test *config.Test, httpConfig *config.Http) {
 	var m sync.Mutex
 	c := sync.NewCond(&m)
 
